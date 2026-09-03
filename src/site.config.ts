d47acf1883b64cb2019b2b66c4c9aa4ba19744bd
celmacioglu <4d2yrhhdhj@privaterelay.appleid.com>
2026-09-03 11:43:01 -0400

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,19 +3,15 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
-	url: "https://astro-cactus.chriswilliams.dev/",
-	/*
+  url: "https://celmacioglu.github.io/",	/*
 		- Used to construct the meta title property found in src/components/BaseHead.astro L:11
 		- The webmanifest name found in astro.config.ts L:42
 		- The link value found in src/components/layout/Header.astro L:35
 		- In the footer found in src/components/layout/Footer.astro L:12
 	*/
-	title: "Astro Cactus",
-	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
-	// Used as the default description meta property and webmanifest description
-	description: "An opinionated starter theme for Astro",
-	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
+  title: "Ceyhun Elmacioglu",	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+  author: "Ceyhun Elmacioglu",	// Used as the default description meta property and webmanifest description
+  description: "Mathematics Ph.D. student at Stony Brook University",	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
   ogLocale: "en_GB",
